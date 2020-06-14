1e70ef3ca2245d59a5fa7a9bef812e7e36b99beb
Your Name <you@example.com>
2020-06-14 16:19:11 -0300

    .

--- Providers/CompanyWinmaxServiceProvider.php
+++ Providers/CompanyWinmaxServiceProvider.php
@@ -8,13 +8,22 @@ use Illuminate\Database\Eloquent\Factory;
 class CompanyWinmaxServiceProvider extends ServiceProvider
 {
     /**
+     * @var string $moduleName
+     */
+    protected $moduleName = 'CompanyWinmax';
+
+    /**
+     * @var string $moduleNameLower
+     */
+    protected $moduleNameLower = 'companywinmax';
+
+    /**
      * Boot the application events.
      *
      * @return void
      */
     public function boot()
     {
-        $this->registerTranslations();
         $this->registerConfig();
         $this->registerViews();
         $this->registerFactories();
@@ -39,11 +48,17 @@ class CompanyWinmaxServiceProvider extends ServiceProvider
     protected function registerConfig()
     {
         $this->publishes([
+            module_path($this->moduleName, 'Config/config.php') => config_path($this->moduleNameLower . '.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            module_path($this->moduleName, 'Config/config.php'), $this->moduleNameLower
+        );
+        /*$this->publishes([
             module_path('CompanyWinmax', 'Config/config.php') => config_path('companywinmax.php'),
         ], 'config');
         $this->mergeConfigFrom(
             module_path('CompanyWinmax', 'Config/config.php'), 'companywinmax'
-        );
+        );*/
     }
 
     /**
@@ -53,7 +68,17 @@ class CompanyWinmaxServiceProvider extends ServiceProvider
      */
     public function registerViews()
     {
-        $viewPath = resource_path('views/modules/companywinmax');
+        $viewPath = resource_path('views/modules/' . $this->moduleNameLower);
+
+        $sourcePath = module_path($this->moduleName, 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ], ['views', $this->moduleNameLower . '-module-views']);
+
+        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->moduleNameLower);
+
+        /*$viewPath = resource_path('views/modules/companywinmax');
 
         $sourcePath = module_path('CompanyWinmax', 'Resources/views');
 
@@ -63,24 +88,10 @@ class CompanyWinmaxServiceProvider extends ServiceProvider
 
         $this->loadViewsFrom(array_merge(array_map(function ($path) {
             return $path . '/modules/companywinmax';
-        }, \Config::get('view.paths')), [$sourcePath]), 'companywinmax');
+        }, \Config::get('view.paths')), [$sourcePath]), 'companywinmax');*/
     }
 
-    /**
-     * Register translations.
-     *
-     * @return void
-     */
-    public function registerTranslations()
-    {
-        $langPath = resource_path('lang/modules/companywinmax');
-
-        if (is_dir($langPath)) {
-            $this->loadTranslationsFrom($langPath, 'companywinmax');
-        } else {
-            $this->loadTranslationsFrom(module_path('CompanyWinmax', 'Resources/lang'), 'companywinmax');
-        }
-    }
+   
 
     /**
      * Register an additional directory of factories.
@@ -90,8 +101,12 @@ class CompanyWinmaxServiceProvider extends ServiceProvider
     public function registerFactories()
     {
         if (! app()->environment('production') && $this->app->runningInConsole()) {
-            app(Factory::class)->load(module_path('CompanyWinmax', 'Database/factories'));
+            app(Factory::class)->load(module_path($this->moduleName, 'Database/factories'));
         }
+
+        /*if (! app()->environment('production') && $this->app->runningInConsole()) {
+            app(Factory::class)->load(module_path('CompanyWinmax', 'Database/factories'));
+        }*/
     }
 
     /**
@@ -102,5 +117,16 @@ class CompanyWinmaxServiceProvider extends ServiceProvider
     public function provides()
     {
         return [];
+    }
+
+    private function getPublishableViewPaths(): array
+    {
+        $paths = [];
+        foreach (\Config::get('view.paths') as $path) {
+            if (is_dir($path . '/modules/' . $this->moduleNameLower)) {
+                $paths[] = $path . '/modules/' . $this->moduleNameLower;
+            }
+        }
+        return $paths;
     }
 }
